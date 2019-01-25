4a55e7c3aed07481d5f1894bb0b6f1d0aedae157
SquallzFunk <39074621+SquallzFunk@users.noreply.github.com>
2019-01-24 19:59:52 -0500

    added jump animation

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
     public Animator animator;
+    public float playerSpeed;
+    public Vector2 jumpHeight;
+    public bool jump = false;
     
     void Update()
     {
@@ -12,5 +15,19 @@ public class BasicMovement : MonoBehaviour
         
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
         transform.position = transform.position + horizontal * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
     }
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        animator.SetBool("IsJumping", false); 
+    }
+
 }
