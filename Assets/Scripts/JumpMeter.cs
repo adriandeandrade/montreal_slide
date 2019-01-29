d221fbb6180f0d7c6b4beabfac30d35cda5905de
Adrian De Andrade <adriandeandrade98@gmail.com>
2019-01-29 18:41:38 -0500

    Added comments

--- Assets/Scripts/JumpMeter.cs
+++ Assets/Scripts/JumpMeter.cs
@@ -30,28 +30,28 @@ public class JumpMeter : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && !playerController.isJumping)
+        if(Input.GetKeyDown(KeyCode.Space) && !playerController.isJumping) // Check if we press the space bar and if the player is not jumping.
         {
-            StartCoroutine(CalculateJumpForce());
-            isCalculatingJump = true;
+            StartCoroutine(CalculateJumpForce()); // We start to coroutine for calculating the jump.
+            isCalculatingJump = true; // We let the class know we are calculating a jump.
         }
     }
 
     IEnumerator CalculateJumpForce()
     {
-        while (Input.GetKey(KeyCode.Space))
+        while (Input.GetKey(KeyCode.Space)) // We run the code until we release the space bar.
         {
-            if(jumpForceAmount >= 1f)
+            if(jumpForceAmount >= 1f) // Since the scrollbar value only goes from 0-1, we want to make sure we dont go over 1.
             {
                 isCalculatingJump = false;
-                playerController.Jump(jumpForceAmount * jumpForceMultiplier);
-                jumpForceAmount = 0f;
-                scrollBar.value = 0f;
+                playerController.Jump(jumpForceAmount * jumpForceMultiplier);  // But if we do reach that value, we do the jump automatically.
+                jumpForceAmount = 0f; // Reset the jump force value to 0.
+                scrollBar.value = 0f; // Reset the scroll bar value to 0 (visual).
                 yield break;
             }
 
-            jumpForceAmount += jumpForceIncrement * Time.deltaTime;
-            scrollBar.value = jumpForceAmount;
+            jumpForceAmount += jumpForceIncrement * Time.deltaTime; // Increment jump force amount by the multiplier we set.
+            scrollBar.value = jumpForceAmount; // Set the scrollbar value to the jump force amount each deltaTime second.
             yield return new WaitForSeconds(Time.deltaTime);
         }
 
