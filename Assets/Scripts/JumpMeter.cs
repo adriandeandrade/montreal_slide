95c06e2940a47c8a230eaa5db0e6d6322334deb6
Adrian De Andrade <redfoxhint4@hotmail.com>
2019-01-29 00:14:24 -0500

    Started work on jump meter

---
+++ Assets/Scripts/JumpMeter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JumpMeter : MonoBehaviour
+{
+    public Scrollbar scrollBar;
+
+    public float jumpValue = 0f;
+    public float meterIncrement;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        UpdateJumpMeter();
+    }
+
+    private void UpdateJumpMeter()
+    {
+        bool spaceBar = Input.GetKey(KeyCode.Space);
+
+        if(spaceBar)
+        {
+            jumpValue += meterIncrement * Time.deltaTime;
+            scrollBar.value = jumpValue;
+
+            if(jumpValue >= 1)
+            {
+                // Initiate jump automatically
+            }
+        }
+    }
+}
