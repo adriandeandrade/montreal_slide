00125bd76ae3e1b085ff9d6e716b16e8f9593f1e
Adrian De Andrade <redfoxhint4@hotmail.com>
2019-02-07 15:55:48 -0500

    Added main menu and scene fade

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    private LevelFader levelFader;
+    [SerializeField] private GameObject helpMenu;
+
+    private void Awake()
+    {
+        levelFader = FindObjectOfType<LevelFader>();
+    }
+
+    public void Play()
+    {
+        levelFader.FadeToLevel(1);
+    }
+
+    public void Help()
+    {
+        helpMenu.SetActive(true);
+    }
+
+    public void ExitHelpMenu()
+    {
+        helpMenu.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
